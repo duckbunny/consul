7147d0399a2284c59a8657ee856e204a084d4bf6
Jason Smith <jason@gramble.com>
2016-01-14 11:40:48 +0100

    added some additiuonal docs

--- doc.go
+++ doc.go
@@ -9,14 +9,15 @@ The consul package implements the Pool and Declare interfaces for the Herald pac
 
 The package takes one flag "consul-ttl" to set the time until the service expires from consul using a heartbeat.
 
-This service must be registered with herald.
-
-	consul.Register()
 
 The package utilizies the Default client returned from consul api, but this can be overriden by edited
 
 	ConsulConfig.Config.Address = "192.168.1.56"
 
+This service must be registered with herald.
 
+	consul.Register()
+
+Or you can fall back on the herald service registryi, to register all available services.
 
 */
